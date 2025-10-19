ac46c9505622aaf132637e1c14a73020ba5d69ec
PPavell234 <varvardidl@gmail.com>
2025-10-20 03:46:03 +1000

    верстка1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,64 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
 <template>
-  <div>
-    <h1 className="text-4xl font-bold text-blue-600">
-      Hello Tailwind v4!
-    </h1>
+  <Header />
+  <div class="flex justify-center items-center">
+    <img src="/images/Title1.png" alt="" class="h-150 w-auto" />
   </div>
+
+  <div class="text-white bg-[#262423] min-h-screen p-6 flex justify-center pl-25 ">
+    <div class="pr-14">
+      <ul class="flex flex-col items-start space-y-2">
+        <img src="/images/autor.png" alt="" class="">
+        <li>Автор</li>
+        <li>Per Landin</li>
+        <li>Опубликовано</li>
+        <li>16.10.2025</li>
+
+
+      </ul>
+
+    </div>
+    <div class="pr-1 pl-12 space-y-4 max-w-[900px]">
+      <h1 class="text-2xl">SoulSteel – a new dungeoneering adventure</h1>
+      <p>Evolve your arsenal and conquer the dungeon!</p>
+      <div></div>
+      <p class="mt-10">Overworld caves are great and all with their stalagmites, stalactites, and occasional bats, but
+        have you ever
+        paid a visit to a
+        dungeon? How about a time-based dungeon with foes and obstacles challenging you at every given opportunity? Did
+        I
+        get your
+        attention! Woohoo! </p>
+      <p>Introducing SoulSteel – an ever-expanding multiplayer dungeon crawler from Noxcrew where every corner holds a
+        new
+        adventure, available through the Minecraft Bedrock Edition server list – today! </p>
+      <img src="/images/vi.png" alt="" class="w-full h-auto rounded-xl ">
+      <h1>EXPLORE A DEEP DUNGEON</h1>
+      <p>In SoulSteel, you assume the role of a daring, gear-craving relic hunter with the sole mission of exploring
+        treacherous dungeons
+        in the pursuit of glory and treasure. Your spoils give you resources and materials that allow you to evolve your
+        character, unlock
+        new abilities, grow in strength, and take on even tougher challenges. The more you extract, the better the
+        rewards! </p>
+      <p> </p>
+      <p>Time is your greatest enemy. Or well, one of many enemies, actually! The undiscovered challenges held within
+        these dungeons
+        will test even the most seasoned adventurer with perplexing puzzles, treacherous traps, and mighty mobs hungry
+        for a fight – all
+        with the ability to end your journey suddenly and swiftly.   </p>
+      <p></p>
+      <p>But with risk comes the promise of rewards! Fortune favours the bold – those that venture the deepest will get
+        greater rewards,
+        and more plentiful resources. And better gear is an absolute necessity if you plan on conquering the progressive
+        challenges that
+        lie deep within these winding halls and corridors. </p>
+
+    </div>
+
+  </div>
+
+
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class="bg-[#262423] w-full h-10 flex items-center">
+        <img src="/public/images/logo.png" alt="logo" class="ml-10 h-8" />
+    </header>
+</template>
