836a350c8df5fe8f6fb4511877993b48622cc75b
PPavell234 <varvardidl@gmail.com>
2025-10-21 17:34:57 +1000

    Стили

--- src/App.vue
+++ src/App.vue
@@ -106,6 +106,18 @@ const selectIcon = (index) => {
 
 </script>
 
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+
 
 
 <template>
