67acc74eb39372532d8f4f9dd0d8ee9f7798c4ee
PPavell234 <varvardidl@gmail.com>
2025-11-04 17:57:59 +1000

    Добавление страниц

---
+++ src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-[#262423] text-white">
+        <h1 class="text-3xl font-bold">🧾 Register Page</h1>
+    </div>
+</template>
