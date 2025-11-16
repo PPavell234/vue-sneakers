1909e13b64a248e54b36ab860489466a8a3ed6aa
PPavell234 <varvardidl@gmail.com>
2025-11-16 23:43:11 +1000

    Товар чекбокс

--- client/src/views/Coin/CoinHome.vue
+++ client/src/views/Coin/CoinHome.vue
@@ -81,33 +81,42 @@ const checked = ref(false)
                 <p class="text-lg font-semibold">ВЫБЕРИТЕ НАБОР</p>
             </div>
 
-            <!-- Один товар -->
-            <div class="flex flex-col w-full max-w-[250px] bg-[#1f1e1d] text-white p-4 rounded-lg gap-12">
 
-                <div class="flex flex-col items-center gap-3">
-                    <img src="/images/CoinHome/coins.png" alt="" class="" />
-                    <p class="text-xl font-bold">320</p>
-                </div>
+            <!-- Товары -->
+            <div class="flex flex-row justify-center gap-4 mb-1">
+                <!-- Один товар -->
+                <div class="flex flex-col w-full max-w-[250px] bg-[#1f1e1d] text-white pl-15 pr-15 rounded-lg gap-12">
 
-                <!-- Разделитель -->
-                <div class="">
-                    <hr class="border-t border-[#33302F]" />
-                </div>
+                    <div class="flex flex-col items-center gap-3">
+                        <img src="/images/CoinHome/coins.png" alt="" class="" />
+                        <p class="text-xl font-bold">320</p>
+                    </div>
 
-                <!-- Чекбокс-кнопка -->
-                <div class="flex flex-row justify-center gap-2.5 mb-1">
-                    <label class="cursor-pointer inline-flex flex-col items-center">
-                        <input type="checkbox" v-model="checked1" class="hidden" />
-                        <img :src="checked1 ? '/images/CoinHome/buttch1.png' : '/images/CoinHome/buttch2.png'"
-                            class="w-10 h-10" alt="checkbox" />
-                    </label>
-                    <div class="py-2">
-                        <p>1200руб</p>
+                    <!-- Разделитель -->
+                    <div class="-mt-1">
+                        <hr class="border-t border-[#33302F]" />
+                    </div>
+
+                    <!-- Чекбокс-кнопка -->
+                    <div class="flex flex-row justify-center gap-2.5 mb-5 -mt-6">
+                        <label class="cursor-pointer inline-flex flex-col items-center">
+                            <input type="checkbox" v-model="checked1" class="hidden" />
+                            <img :src="checked1 ? '/images/CoinHome/buttch1.png' : '/images/CoinHome/buttch2.png'"
+                                class="w-8 h-8" alt="checkbox" />
+                        </label>
+                        <div class="px-1">
+                            <p>1200руб</p>
+                        </div>
+
                     </div>
 
                 </div>
-
             </div>
+
+
+
+
+
 
         </div>
 
