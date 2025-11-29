779657f72c766395e76909761cd803b659f94871
PPavell234 <varvardidl@gmail.com>
2025-11-29 22:31:55 +1000

    Если страницы нету выдает ошибку 404

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,16 +6,17 @@ import LoginProf from '@/views/LoginProf.vue'
 import CoinHome from '@/views/Coin/CoinHome.vue'
 import Error404 from '@/views/Error/Error404.vue'
 
-//  Каждый раз при создании страницы регистрации ее тут
+// Страницы, которые должны перенаправлять на 404
+const emptyPages = ['/old-page', '/deprecated', '/under-construction']
 
 const routes = [
   {
-    path: '/', // главная страница
+    path: '/',
     name: 'Home',
     component: Home,
   },
   {
-    path: '/register', // страница регистрации
+    path: '/register',
     name: 'Register',
     component: Register,
   },
@@ -31,12 +32,18 @@ const routes = [
   },
   {
     path: '/coinHome',
-    name: 'coinHome',
+    name: 'CoinHome',
     component: CoinHome,
   },
   {
     path: '/error404',
-    name: 'error404',
+    name: 'Error404',
+    component: Error404,
+  },
+  // Все остальные маршруты -> 404
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
     component: Error404,
   },
 ]
@@ -44,6 +51,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  // Если маршрут в списке пустых страниц - перенаправляем на 404
+  if (emptyPages.includes(to.path)) {
+    next('/error404')
+    return
+  }
+
+  next()
 })
 
 export default router
